3b9e5a4f4761fb592dbab7d0a96a4c4a237d9911
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2026-06-27 20:08:53 +0200

    updte

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,6 @@
-export const server = "http://localhost:8000/api";
+export const server = "https://todjom.greenlyfes.com/api";
 
-export const backend_url = "http://localhost:8000/api";
+export const backend_url = "https://todjom.greenlyfes.com/api";
 
-export const api = "http://localhost:8000/api";
-export const image = "http://localhost:8000";
+export const api = "https://todjom.greenlyfes.com/api";
+export const image = "https://todjom.greenlyfes.com";
