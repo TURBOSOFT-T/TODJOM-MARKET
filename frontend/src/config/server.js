bcd95aa33eee04a5f6696dcfda2c9000a358717f
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2026-06-28 11:52:59 +0200

    update

--- frontend/src/config/server.js
+++ frontend/src/config/server.js
@@ -1,3 +1,19 @@
-export const server = "http://localhost:9000/";
+/* export const server = "http://localhost:9000/";
 
 export const backend_url = "http://localhost:8000/"
+ */
+export const server = "https://todjom.greenlyfes.com/api";
+
+export const backend_url = "https://todjom.greenlyfes.com/api";
+
+export const api = "https://todjom.greenlyfes.com/api";
+export const image = "https://todjom.greenlyfes.com";
+
+
+/* 
+export const server ="https://localhost:8000/api";
+
+
+
+
+ export const image ="https://localhost:8000/api"; */
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -4,3 +4,12 @@ export const backend_url = "https://todjom.greenlyfes.com/api";
 
 export const api = "https://todjom.greenlyfes.com/api";
 export const image = "https://todjom.greenlyfes.com";
+
+
+/* 
+export const server ="https://localhost:8000/api";
+
+
+
+
+ export const image ="https://localhost:8000/api"; */
